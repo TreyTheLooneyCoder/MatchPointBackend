b6ad369ed537e80cf6f0845c8a2c2256c19f895d
Tvang93 <vangt39@gmail.com>
2025-05-20 10:51:49 -0700

    adding additional properties

--- Models/LocationPropertiesModel.cs
+++ Models/LocationPropertiesModel.cs
@@ -7,13 +7,16 @@ namespace MatchPointBackend.Models
 {
     public class LocationPropertiesModel
     {
-        public int Id {get; set;}
-        public int LocationId {get; set;}
-        public string? CourtName {get; set;}
-        public float CourtRating {get; set;} 
-        public float SafetyRating {get; set;} 
-        public List<string>? Conditions {get; set;}
-        public List<string>? Amenities {get; set;}
-        public ICollection<CommentModel>? Comments {get;set;}
+        public int Id { get; set; }
+        public int LocationId { get; set; }
+        public string? CourtName { get; set; }
+        public float CourtRating { get; set; }
+        public float SafetyRating { get; set; }
+        public List<string>? Conditions { get; set; }
+        public List<string>? Amenities { get; set; }
+        public ICollection<CommentModel>? Comments { get; set; }
+        public int Surface { get; set; }
+        public string[]? Images { get; set; }
+        public bool isDeleted { get; set; }
     }
 }
